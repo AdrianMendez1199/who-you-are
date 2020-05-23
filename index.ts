d079d3b809b268deb4d7eca20b4b50b95a963f13
Adrian Mendez <mendezadrian149@gmail.com>
2020-05-23 10:54:26 -0400

    working on stucture

--- index.ts
+++ index.ts
@@ -1,10 +1,16 @@
 import { ApolloServer } from 'apollo-server';
 import { resolvers, typeDefs } from './src/graphql';
 
-const server = new ApolloServer({
+process.on('unhandledRejection', (reason, promise): void => {
+  console.log(`reason: ${reason}, promise: ${promise}`);
+});
+
+const server: ApolloServer = new ApolloServer({
   typeDefs,
   resolvers,
+  context: (request): object => ({ request }),
 });
+
 
 server.listen().then(({ url }): void => {
   console.log(`Server running ${url}`);
